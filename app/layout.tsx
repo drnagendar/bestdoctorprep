3db38e4c0c4330ce80a19641ea28886d987bc864
DrNagendar <drnagendar@gmail.com>
2025-07-02 07:11:42 +0530

    Fix: Remove duplicate RootLayout and Header import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,3 @@
-import Header from './components/Header';
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        <Header />
-        {children}
-      </body>
-    </html>
-  );
-}
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "./components/Header";
